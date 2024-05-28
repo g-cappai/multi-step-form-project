1c38771d3a01c1e3aa38a6116c59dc5b7ef5c8c6
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-05-28 20:55:56 +0200

    Create Stepper

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+
+type Props = {
+  steps: number;
+  currentStep: number;
+};
+
+export function Stepper({ steps: stepsAmount, currentStep }: Props) {
+  const steps = useMemo(() => new Array(stepsAmount).fill(""), [stepsAmount]);
+
+  return (
+    <div className="stepper">
+      {steps.map((_, index) => {
+        return (
+          <div
+            key={index}
+            className={`step ${currentStep === index ? "step--active" : ""}`}
+          >
+            <div className="step__number">{index + 1}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
