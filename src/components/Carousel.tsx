24c1a40522513609741fc0b46fdf16650a0ed7d4
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-05-28 15:16:23 +0200

    Use BEM naming

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,14 +5,13 @@ type Props = {
 
 export function Carousel({ steps, currentStep }: Props) {
   return (
-    <div className="mx-auto w-3/5 overflow-hidden rounded-md bg-green-950">
+    <div className="carousel">
       <div
-        className={`flex py-4 transition duration-300 ease-out`}
+        className="carousel__content"
         style={{ transform: `translateX(-${currentStep * 100}%)` }}
       >
         {steps.map((step) => (
-          <div className="min-w-full px-4">
-            <h2>{step.title}</h2>
+          <div className="carousel__step">
             {step.content}
           </div>
         ))}
