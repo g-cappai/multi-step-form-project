a7e1acaeab9744e68a874cc3e8eac0c632f9a045
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-05-30 20:09:12 +0200

    Complete validation

--- src/api/validateStep.ts
+++ src/api/validateStep.ts
@@ -1,6 +1,6 @@
 export type StepPayload = {
   stepNumber: number;
-  values: { name: string } | { surname: string } | { address: string };
+  values: { [key: string]: string };
 };
 
 export type StepResponse =
@@ -9,44 +9,32 @@ export type StepResponse =
     }
   | {
       status: "error";
-      errors: {
-        name?: string;
-        surname?: string;
-        address?: string;
-      };
+      errors: { [key: string]: string };
     };
 
 export async function validateStep(
   payload: StepPayload
 ): Promise<StepResponse> {
-  const isStepValid = (payload: StepPayload) => {
-    if (payload.stepNumber === 0) {
-      return (payload.values as { name: string }).name === "Mario";
-    }
-
-    if (payload.stepNumber === 1) {
-      return (payload.values as { surname: string }).surname === "Rossi";
-    }
-
-    if (payload.stepNumber === 2) {
-      return (payload.values as { address: string }).address === "Via Roma, 1";
-    }
-
-    return false;
+  const getErrors = (payload: StepPayload): string[][] => {
+    return Object.entries(payload.values)
+      .map(([key, value]) => {
+        if (!value) {
+          return [key, "This field is required!"];
+        }
+      })
+      .filter(Boolean) as string[][];
   };
 
   return new Promise((resolve) => {
+    const errors = getErrors(payload);
+    console.log({ errors });
     setTimeout(() => {
-      if (isStepValid(payload)) {
+      if (!errors.length) {
         resolve({ status: "success" });
       } else {
         resolve({
           status: "error",
-          errors: {
-            name: payload.stepNumber === 0 ? "Invalid name" : undefined,
-            surname: payload.stepNumber === 1 ? "Invalid surname" : undefined,
-            address: payload.stepNumber === 2 ? "Invalid address" : undefined,
-          },
+          errors: Object.fromEntries(errors),
         });
       }
     }, 1000);
