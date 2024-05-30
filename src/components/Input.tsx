ffe442e9282f05601871db7bebec4986e4457ea9
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-05-30 03:18:30 +0200

    Adapt Input to react hook form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,35 +1,37 @@
-type InputProps = {
+import {
+  FieldValues,
+  UseControllerProps,
+  useController,
+} from "react-hook-form";
+
+type InputProps<T extends FieldValues> = {
   label: string;
   name: string;
-  value: string;
-  errorMessage?: string;
-  onChange: (value: string) => void;
-};
+} & { controller: UseControllerProps<T> };
 
-export function Input({
+export function Input<T extends FieldValues>({
   label,
   name,
-  value,
-  errorMessage,
-  onChange,
-}: InputProps) {
-  const hasError = Boolean(errorMessage);
-
+  controller,
+}: InputProps<T>) {
+  const { field, fieldState } = useController(controller);
   return (
     <div className="input">
       <label className="input__label" htmlFor={name}>
         {label}
       </label>
       <input
-        className="input__field"
+        className={`input__field ${
+          fieldState.invalid ? "input__field--invalid" : ""
+        }`}
         name={name}
         type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
+        value={field.value}
+        onChange={(e) => field.onChange(e.target.value)}
       />
-      {hasError && (
+      {fieldState.invalid && (
         <div className="input__error" role="alert">
-          {errorMessage}
+          {fieldState.error?.message}
         </div>
       )}
     </div>
