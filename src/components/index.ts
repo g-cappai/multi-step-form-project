be92e2ab1ffd15a8913fc26c42b4ef13f29df2f9
Gianluca Cappai <gcappai.dev@@gmail.com>
2024-05-30 19:24:30 +0200

    Delete controls components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { Input } from "./Input";
 export { Carousel } from "./Carousel";
 export { Stepper } from "./Stepper";
-export { Controls } from "./Controls";
 export { Button } from "./Button";
 export { Accordion } from "./Accordion";
